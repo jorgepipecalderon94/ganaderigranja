cebfbc392a7b4dfc051ba96cd96e07cf378f223a
Jorge calderon <jorgepipecalderon94@gmail.com>
2026-03-30 18:16:36 -0500

    Primer commit - ajustes

---
+++ guardar_leche.php
@@ -0,0 +1,38 @@
+<?php
+include("conexion.php");
+
+// EVITA ERROR SI ENTRAN DIRECTO
+if($_SERVER["REQUEST_METHOD"] != "POST"){
+    echo "Acceso no permitido";
+    exit();
+}
+
+// CAPTURA
+$vaca = $_POST['numero_devaca'] ?? "";
+$mes = $_POST['mes'] ?? "";
+
+$lunes = $_POST['lunes'] ?? 0;
+$martes = $_POST['martes'] ?? 0;
+$miercoles = $_POST['miercoles'] ?? 0;
+$jueves = $_POST['jueves'] ?? 0;
+$viernes = $_POST['viernes'] ?? 0;
+$sabado = $_POST['sabado'] ?? 0;
+$domingo = $_POST['domingo'] ?? 0;
+
+$total = $_POST['total_semanal'] ?? 0;
+
+// INSERTAR
+$sql = "INSERT INTO venta_leche 
+(numero_devaca, mes, lunes, martes, miercoles, jueves, viernes, sabado, domingo, total_semanal)
+VALUES 
+('$vaca','$mes','$lunes','$martes','$miercoles','$jueves','$viernes','$sabado','$domingo','$total')";
+
+if($conexion->query($sql)){
+    echo "<script>
+    alert('Guardado correctamente');
+    window.location='registro_leche_diaria.html';
+    </script>";
+} else {
+    echo "Error: " . $conexion->error;
+}
+?>
---
+++ guardar_usuario.php
@@ -0,0 +1,26 @@
+<?php
+include("conexion.php");
+
+// Recibir datos
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$documento = $_POST['documento'];
+$correo = $_POST['correo'];
+$password = $_POST['password'];
+$rol = $_POST['rol'];
+
+// Encriptar contraseña
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+// Insertar datos
+$sql = "INSERT INTO registro_usuario (Nombre, Apellido, Documento, Correo, Contraseña, Rol)
+VALUES ('$nombre', '$apellido', '$documento', '$correo', '$passwordHash', '$rol')";
+
+if ($conexion->query($sql) === TRUE) {
+    header("Location: ingreso_sistema.html"); // REDIRIGE AL LOGIN
+} else {
+    echo "Error: " . $conexion->error;
+}
+
+$conexion->close();
+?>
